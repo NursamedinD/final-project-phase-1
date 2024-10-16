b22dccd7ff24d9d18f4306d794b336ca32c18a8f
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-10-16 12:17:57 +0300

    added berry variable

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const canvas = document.getElementById("gameContainer");
+const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const scoreText = document.querySelector(".scoreBar");
 const resetBtn = document.querySelector("#resetBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
 const startBtn = document.querySelector('#startBtn');
-  
+
 let gameinterval;
 let started
 let paused = false;
-let snake = [{x: 10, y: 10}];
+let snake = [{ x: 10, y: 10 }];
 let score = 0;
 let snakeSpeed = 5;
 let gridSize = 20;
-// let snakeColor = "red"
-// let foodColor = "yellow"
+let berry = {x: Math.floor(Math.random() * 25), y: Math.floor(Math.random() * 25)};
+
 
 const fetchSettings = async () => {
   try {
@@ -30,7 +30,7 @@ const fetchSettings = async () => {
 
 function gameStart() {
   if (!started)
-  gameinterval = setInterval(updateGame, 1000 / snakeSpeed);
+    gameinterval = setInterval(updateGame, 1000 / snakeSpeed);
   started = true;
 }
 
@@ -44,12 +44,13 @@ function gamePause() {
     pauseBtn.textContent = "Pause"
     paused = false;
   }
+  pauseBtn.addEventListener("click", gamePause);
 }
 
 function updateGame() {
   console.log("Game running...");
 }
 
-pauseBtn.addEventListener("click", gamePause);
+
 
 gameStart();
